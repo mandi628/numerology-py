6d18fd91e96033e7c59df892e493f1ae1c48b5ce
mandi628 <mandi628.codes@gmail.com>
2024-08-07 14:01:03 -0400

    Version 0.6.0 - Worked out Pinnacles calcs in test file

--- test/cycles-test.py
+++ test/cycles-test.py
@@ -29,23 +29,82 @@ def make_list(entry):
         ent_int.append(int(e))
     return ent_int
 
+def birth_s(birth):
+    birth_s = []
+    day = simplify(make_list(birth[3:5]))
+    birth_s.append(day)
+    mon = simplify(make_list(birth[:2]))
+    birth_s.append(mon)
+    by = simplify(make_list(birth[6:]))
+    birth_s.append(by)
+    return birth_s
+
+def cycles(birth):
+    cycles = []
+    mc1 = simplify(make_list(str(birth_s(birth)[1])))
+    cycles.append(mc1)
+
+    mc2 = simplify(make_list(str(birth_s(birth)[0])))
+    cycles.append(mc2)
+
+    mc3 = simplify(make_list(str(birth_s(birth)[2])))
+    cycles.append(mc3)
+    return cycles
+
+def pinnacles(birth):
+    pinnacles = []
+
+    pn1 = simplify(make_list(str(birth_s(birth)[0] + birth_s(birth)[1])))
+    pinnacles.append(pn1)
+    pn2 = simplify(make_list(str(birth_s(birth)[0] + birth_s(birth)[2])))
+    pinnacles.append(pn2)
+    pn3 = simplify(make_list(str(pn1 + pn2)))
+    pinnacles.append(pn3)
+    pn4 = simplify(make_list(str(birth_s(birth)[1] + birth_s(birth)[2])))
+    pinnacles.append(pn4)
+    return pinnacles
+
+def challenges(birth):
+    challenges = []
+
+    ch1 = simplify(make_list(str(abs(birth_s(birth)[0] - birth_s(birth)[1]))))
+    challenges.append(ch1)
+    ch2 = simplify(make_list(str(abs(birth_s(birth)[0] - birth_s(birth)[2]))))
+    challenges.append(ch2)
+    ch3 = simplify(make_list(str(abs(ch1 - ch2))))
+    challenges.append(ch3)
+    ch4 = simplify(make_list(str(abs(birth_s(birth)[1] - birth_s(birth)[2]))))
+    challenges.append(ch4)
+    return challenges
+
+def lp_no(birth):
+    lpath = simplify(make_list(birth))
+    return lpath
+
+def pin_ages(birth):
+    lfpn = lp_no(birth)
+    p_ages = []
+    age1 = 36 - lfpn
+    p_ages.append(age1)
+    age2 = age1 + 9
+    p_ages.append(age2)
+    age3 = age2 + 9
+    p_ages.append(age3)
+    return p_ages
 
 birth = "06 28 1976"
+print("Birth: ", birth)
 
-cycles = []
-mc1 = birth[:2]
-mc1_list = make_list(mc1)
-mc1s = simplify(mc1_list)
-cycles.append(mc1s)
+life_path = lp_no(birth)
+print("Life Path: ", life_path)
+maj_cycles = cycles(birth)
+print("Major cyles: ", maj_cycles)
 
-mc2 = birth[3:5]
-mc2_list = make_list(mc2)
-mc2s = simplify(mc2_list)
-cycles.append(mc2s)
+pinn = pinnacles(birth)
+print("Pinnacles: ", pinn)
 
-mc3 = birth[6:]
-mc3_list = make_list(mc3)
-mc3s = simplify(mc3_list)
-cycles.append(mc3s)
+chal = challenges(birth)
+print("Challenges: ", chal)
 
-print(cycles)
+pinn_ages = pin_ages(birth)
+print("Pinnacle Ages: ", pinn_ages)
